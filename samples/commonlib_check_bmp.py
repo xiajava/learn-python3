abcc620a7155f7d00d72e3871df6a2f5ea3af110
xia <javaxia@outlook.com>
2017-07-23 21:53:53 +0800

    Update commonlib_check_bmp.py

--- samples/commonlib_check_bmp.py
+++ samples/commonlib_check_bmp.py
@@ -13,14 +13,14 @@ def is_bmp(d):
         with open(d, 'rb') as f:
             info = struct.unpack('<ccIIIIIIHH', f.read(30))
         # size, move, Header = info[2], info[4], info[5]
-        type, Reserved_bit, height, high, one, colors = info[0:2], info[3], info[6], info[7], info[8], info[9]
+        ptype, Reserved_bit, width, height, one, colors = info[0:2], info[3], info[6], info[7], info[8], info[9]
         print('bmp_info:', info)
-        if Reserved_bit == 0 and one == 1 and (type == (b'B', b'M') or type == (b'B', b'A')):
-            if type == (b'B', b'M'):
+        if Reserved_bit == 0 and one == 1 and (ptype == (b'B', b'M') or ptype == (b'B', b'A')):
+            if ptype == (b'B', b'M'):
                 print('Type: Windows bitmap')
             else:
                 print('Type: OS/2 bitmap')
-            print('Image Size: %s*%s' % (height, high))
+            print('Image Size: %s*%s' % (width, height))
             print('Colors:', colors)
     except:
         print('not bitmap')
